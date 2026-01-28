1b623bf8d13e14a770868353cda46e6e00836ca6
Apollo <amporto10@gmail.com>
2026-01-28 16:49:14 -0300

    Criação de usuário

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 // Decorator - Recurso de anotação/decoração para modificar classes, funções, propriedades, etc. em tempo de execução 
 
 // Module - Agrupador lógico de componentes relacionados (controllers, services, etc.) que podem ser referenciados
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
